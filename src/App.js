b4dd85884eece65d17c62a90e45f0255e980506b
Bal Elsada Hasun <fouadhason@gmail.com>
2022-09-28 16:16:38 +0300

    finish all button action and design style

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import { person } from './Data'
+import { person } from './Data/Data'
 import DateCuont from './Components/DateCuont';
 import DatesList from './Components/DatesList';
 import DateAction from './Components/DateAction';
 
 import {Container} from "react-bootstrap";
+import { useEffect, useState } from 'react';
 
 function App() {
+  
+  const [personData, setPersonData] = useState(person);
+
+  // delet and show all data with click button
+  const onDelete = () => setPersonData([])
+  const onView = () => setPersonData(person)
+
+  // delete all data when the page is loading
+  useEffect(() => { setPersonData([]) },[])
 
   return (
     <div className="font color-body ">
       <Container className='py-5'>
 
-        <DateCuont person={person}/>
-        <DatesList person={person}/>
-        <DateAction person={person}/>
+        <DateCuont person={personData} />
+        <DatesList person={personData } />
+        <DateAction deleteData={onDelete} viewData={onView}/>
 
       </Container>
 
--- src/Components/DateAction.js
+++ src/Components/DateAction.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
 
-const DateAction = ({person}) => {
+const DateAction = ({deleteData, viewData}) => {
   return (
     <Row className='justify-content-center my-3'>
           <Col ms='8' className="d-flex justify-content-between">
-            <button className='btn-style p-2'> امسح البيانات</button>
-            <button className='btn-style p-2'>عرض البيانات</button>
+            <button onClick={deleteData}  className='btn-style p-2'> امسح البيانات</button>
+            <button onClick={viewData} className='btn-style p-2'>عرض البيانات</button>
           </Col>
         </Row>
   )
